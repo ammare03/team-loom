ef5ffefbf90ba5cecbde3bbcb24b56258d2770e0
Ammar Engineer <ammar.f.engineer@outlook.com>
2025-06-30 12:54:07 +0530

    feat: enhance Sidebar component with collapsible functionality and add SidebarLink for navigation

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,21 +1,43 @@
 "use client";
 
-import { LockIcon } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { Button } from "@/components/ui/button";
+import { setIsSidebarCollapsed } from "@/state";
+import { Home, LockIcon, LucideIcon, X } from "lucide-react";
 import Image from "next/image";
-import React, { useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Sidebar() {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
   return (
-    <div className="fixed z-40 flex h-[100%] w-64 flex-col justify-between overflow-y-auto bg-white shadow-xl transition-all duration-300 dark:bg-black">
-      <div className="flex h-[100%] w-full flex-col justify-start">
+    <div
+      className={`fixed z-40 flex h-[100%] flex-col justify-between overflow-y-auto bg-white shadow-xl transition-all duration-300 dark:bg-black ${isSidebarCollapsed ? "hidden w-0" : "w-64"}`}
+    >
+      <div className="flex h-full w-full flex-col justify-start">
         {/* Top Logo */}
-        <div className="min-g-[50px] z-50 flex w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
           <div className="text-xl font-bold text-gray-800 dark:text-white">
             TeamLoom
           </div>
+          {isSidebarCollapsed ? null : (
+            <Button
+              className="border-none bg-transparent py-3 shadow-none hover:bg-gray-100 dark:hover:bg-gray-800"
+              onClick={() =>
+                dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+              }
+            >
+              <X className="size-6 text-gray-500 hover:text-gray-700 dark:text-white dark:hover:text-gray-300" />
+            </Button>
+          )}
         </div>
         {/* Team */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
@@ -37,9 +59,50 @@ export default function Sidebar() {
           </div>
         </div>
         {/* Navbar Links */}
+        <nav className="z-10 w-full">
+          <SidebarLink icon={Home} label="Home" href="/" />
+        </nav>
       </div>
     </div>
   );
+}
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  // isCollapsed?: boolean;
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  // isCollapsed,
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+  const screenWidth = window.innerWidth;
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""}`}
+      >
+        {isActive && (
+          <div className="absolute top-0 left-0 h-full w-[5px] bg-blue-200"></div>
+        )}
+        <Icon className="size-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
 };
-
-
